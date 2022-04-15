d75d14480cc4d6ef461517ce202a528f39ac63e6
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-04-15 12:55:43 -0300

    Create calculomedia.js

---
+++ calculo_media/calculomedia.js
@@ -0,0 +1,9 @@
+ function calcular() {
+            nota1 = document.getElementById('nota1').value
+            nota2 = document.getElementById('nota2').value
+            nota3 = document.getElementById('nota3').value
+
+            resultado = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) / 3
+            document.getElementById('resultado').value = resultado
+            alert ('Cálculo realizado com sucesso!')
+        }
